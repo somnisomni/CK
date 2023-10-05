a286b139de151513ea921ebb34c065620cb27c8d
somni <me@somni.one>
2023-10-05 22:21:28 +0900

    2023-2 OOP Week5 Assignment

---
+++ 2023-2/OOP/Week5/assignment/account.h
@@ -0,0 +1,106 @@
+﻿#pragma once
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+#define ACCOUNT_MAX 100
+
+class Account {
+  private:
+    int accId;     // 계좌번호
+    string regId;  // 주민번호
+    string name;   // 이름
+    int balance;   // 잔액
+  public:
+    Account(int aId, string rId, string n, int b): accId(aId), regId(rId), name(n), balance(b) { }
+    ~Account() {}
+
+    /* Getters */
+    int getAccId() const { return accId; }
+    string getRegId() const { return regId; }
+    string getName() const { return name; }
+    int getBalance() const { return balance; }
+
+    /* Actions */
+    static int generateRandomAccId() { return rand() % 90000 + 10000; }
+    void deposit(int amount) { balance += amount; }
+    bool withdraw(int amount) {
+      if(balance < amount) return false;
+      balance -= amount;
+      return true;
+    }
+    void showAll() const {
+      cout << "성함: " << name << endl;
+      cout << "계좌번호: " << accId << endl;
+      cout << "주민번호: " << regId << endl;
+      cout << "잔액: " << balance << endl;
+    }
+};
+
+class AccountManager {
+  private:
+    Account* accArr[ACCOUNT_MAX];
+    int accNum = 0;
+  public:
+    AccountManager() { }
+    ~AccountManager() {
+      for (int i = 0; i < accNum; i++) {
+        delete accArr[i];
+      }
+    }
+
+    /* Getters */
+    Account* getAccounts() const { return *accArr; }
+    int getAccountNum() const { return accNum; }
+
+    /* Actions */
+    bool addAccount(Account* acc) {
+      if(accNum >= ACCOUNT_MAX) {
+        cout << "[!] 더 이상 계좌를 추가할 수 없습니다." << endl;
+        return false;
+      }
+
+      accArr[accNum++] = acc;
+      return true;
+    }
+    Account* findAccountByAccId(int accId) const {
+      for(int i = 0; i < accNum; i++) {
+        if(accArr[i]->getAccId() == accId) {
+          return accArr[i];
+        }
+      }
+      return NULL;
+    }
+    Account* findAccountByRegId(string regId) const {
+      for(int i = 0; i < accNum; i++) {
+        if(accArr[i]->getRegId() == regId) {
+          return accArr[i];
+        }
+      }
+      return NULL;
+    }
+    void showAll() const {
+      if(accNum <= 0) {
+        cout << "[!] 등록된 계좌가 없습니다." << endl;
+        return;
+      }
+
+      for(int i = 0; i < accNum; i++) {
+        accArr[i]->showAll();
+        cout << endl;
+      }
+    }
+    void showAllSimple() const {
+      if(accNum <= 0) {
+        cout << "[!] 등록된 계좌가 없습니다." << endl;
+        return;
+      }
+
+      for (int i = 0; i < accNum; i++) {
+        cout << "성함: " << accArr[i]->getName() << " / ";
+        cout << "잔액: " << accArr[i]->getBalance() << endl;
+        cout << endl;
+      }
+    }
+};
---
+++ 2023-2/OOP/Week5/assignment/banking.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include "account.h"
+#include "system.h"
+using namespace std;
+
+int main() {
+  srand((unsigned) time(NULL));
+
+  AccountManager manager = AccountManager();
+
+  while(true) {
+    MainMenuSelection s = AppMenu::showMenu();
+    cout << endl;
+
+    switch(s) {
+      case CREATE_ACCOUNT:
+        if(manager.addAccount(AppMenu::showCreateAccountMenu())) {
+          cout << "[*] 계좌가 개설되었습니다." << endl;
+        }
+        break;
+      case DEPOSIT:
+        AppMenu::showDepositMenu(manager);
+        break;
+      case WITHDRAW:
+        AppMenu::showWithdrawMenu(manager);
+        break;
+      case SHOW_ALL:
+        AppMenu::showAllInfoMenu(manager);
+        break;
+      case EXIT:
+        return 0;
+      default:
+        cout << "[!] 잘못된 입력입니다." << endl;
+        cout << endl;
+    }
+  }
+
+  cout << "[*] 종료" << endl;
+  return 0;
+}
