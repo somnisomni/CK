bc015f91bc52713eb7befb49bc6d0f53e28bc8b1
somni <me@somni.one>
2024-03-15 11:51:52 +0900

    2024-1 GN Week2

---
+++ 2024-1/GN/Week2/UDPClient/Program.cs
@@ -0,0 +1,50 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+class Program {
+    private const string SERVERIP = "192.168.110.232";
+    private const int  SERVERPORT = 9000;
+    private const int  BUFFERSIZE = 512;
+    
+    public static void Main(string[] args) {
+        Socket? socket = null;
+        int retval;
+
+        try {
+            socket = new(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+        } catch(Exception e) {
+            Console.Error.WriteLine(e.Message);
+            Environment.Exit(1);
+        }
+        
+        Console.WriteLine(" * UDP Client has started.");
+
+        byte[] buffer = new byte[BUFFERSIZE];
+        IPEndPoint serverAddr = new(IPAddress.Parse(SERVERIP), SERVERPORT);
+        while(true) {
+            Console.Write("Input> ");
+            string input = Console.ReadLine()!;
+            if(input.Length == 0) break;
+            
+            try {
+                byte[] data = Encoding.Default.GetBytes(input);
+                int size = data.Length;
+                if(size > BUFFERSIZE) size = BUFFERSIZE;
+                retval = socket.SendTo(data, size, SocketFlags.None, serverAddr);
+                Console.WriteLine("[UDP Client] {0} bytes sent.", retval);
+                
+                EndPoint peerAddr = new IPEndPoint(IPAddress.Any, 0);
+                retval = socket.ReceiveFrom(buffer, retval, SocketFlags.None, ref peerAddr);
+                if(!((IPEndPoint) peerAddr).Equals(serverAddr)) throw new Exception("Server address mismatch.");
+                string recvData = Encoding.Default.GetString(buffer, 0, retval);
+                Console.WriteLine("[UDP/{0}:{1}] {2}", ((IPEndPoint)peerAddr).Address, ((IPEndPoint)peerAddr).Port, recvData);
+            } catch(Exception e) {
+                Console.Error.WriteLine(e.Message);
+                break;
+            }
+        }
+
+        socket.Close();
+    }
+}
---
+++ 2024-1/GN/Week2/UDPServer/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+class Program {
+    private const int SERVERPORT = 9000;
+    private const int BUFFERSIZE = 512;
+    
+    public static void Main(string[] args) {
+        Socket? socket = null;
+        int retval;
+
+        try {
+            socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            socket.Bind(new IPEndPoint(IPAddress.Any, SERVERPORT));
+        } catch(Exception e) {
+            Console.Error.WriteLine(e.Message);
+            Environment.Exit(1);
+        }
+        
+        Console.WriteLine(" * UDP Server has started.");
+
+        byte[] buffer = new byte[BUFFERSIZE];
+        while(true) {
+            try {
+                EndPoint peerAddr = new IPEndPoint(IPAddress.Any, 0);
+                retval = socket.ReceiveFrom(buffer, BUFFERSIZE, SocketFlags.None, ref peerAddr);
+                
+                string data = Encoding.Default.GetString(buffer, 0, retval);
+                Console.WriteLine("[UDP/{0}:{1}] {2}", ((IPEndPoint)peerAddr).Address, ((IPEndPoint)peerAddr).Port, data);
+
+                socket.SendTo(buffer, retval, SocketFlags.None, peerAddr);
+            } catch(Exception e) {
+                Console.Error.WriteLine(e.Message);
+                break;
+            }
+        }
+        
+        socket.Close();
+    }
+}
