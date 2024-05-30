91574ec5f5295f3d22d6a4d2a6fa12ffbb69c617
somni <me@somni.one>
2024-05-30 19:35:49 +0900

    2024-1 GN Week10 Assignment

---
+++ 2024-1/GN/Week10-Assignment/BinarySerializationTest/BinarySerializationTest.cs
@@ -0,0 +1,68 @@
+﻿public class BinarySerializationTest {
+    /// <summary>
+    /// 테스트용 바이너리 데이터
+    ///
+    /// string "Awesome Data!",
+    ///    int 111,
+    ///  float 23.456f,
+    /// double 19.4464253d,
+    ///   bool true
+    /// </summary>
+    private static readonly byte[] TEST_DATA = new byte[] {
+        0x0D,  // string length
+        0x41, 0x77, 0x65, 0x73, 0x6F, 0x6D, 0x65, 0x20, 0x44, 0x61, 0x74, 0x61, 0x21,  // string content
+        
+        0x6F, 0x00, 0x00, 0x00, // int
+        0xE3, 0xA5, 0xBB, 0x41, // float(single)
+        0x64, 0x9B, 0xAF, 0xED, 0x48, 0x72, 0x33, 0x40, // double
+        0x01                    // bool
+    };
+    // BinaryReader/Writer는 Little-endian으로 데이터를 읽고 쓴다.
+    // 참고: https://learn.microsoft.com/en-us/dotnet/api/system.io.binarywriter.write
+    
+    public static void Main(string[] args) {
+        // 데이터 읽기
+        Console.WriteLine("*** Read binary data");
+        {
+            using var stream = new MemoryStream(TEST_DATA);
+            using var reader = new BinaryReader(stream);
+            
+            string str = reader.ReadString();
+            int    i   = reader.ReadInt32();
+            float  f   = reader.ReadSingle();
+            double d   = reader.ReadDouble();
+            bool   b   = reader.ReadBoolean();
+            
+            Console.WriteLine($"String: {str}");
+            Console.WriteLine($"Int: {i}");
+            Console.WriteLine($"Float: {f}");
+            Console.WriteLine($"Double: {d}");
+            Console.WriteLine($"Bool: {b}");
+        }
+        Console.WriteLine();
+        
+        // 데이터 쓰기
+        Console.WriteLine("*** Write binary data");
+        {
+            using var stream = new MemoryStream();
+            using var writer = new BinaryWriter(stream);
+            
+            const string str = "Another Data!";
+            const int    i   = 222;
+            const float  f   = 563.24f;
+            const double d   = 0.123456789d;
+            const bool   b   = false;
+
+            Console.WriteLine(str); writer.Write("Another Data!");
+            Console.WriteLine(i);   writer.Write(222);
+            Console.WriteLine(f);   writer.Write(563.24f);
+            Console.WriteLine(d);   writer.Write(0.123456789d);
+            Console.WriteLine(b);   writer.Write(false);
+
+            stream.Position = 0;
+            byte[] data = stream.ToArray();
+            Console.WriteLine("0x" + BitConverter.ToString(data).Replace("-", " 0x"));
+        }
+        Console.WriteLine();
+    }
+}
---
+++ 2024-1/GN/Week10-Assignment/JSONSerializationTest/JSONSerializationTest.cs
@@ -0,0 +1,85 @@
+﻿using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+public class JSONSerializationTest {
+    /// <summary>
+    /// 테스트용 JSON 문자열
+    /// </summary>
+    private const string TEST_JSON = "{" +
+                                     "  \"name\": \"John\"," +
+                                     "  \"grade\": 2," +
+                                     "  \"lastAvgScore\": 3.336," +
+                                     "  \"extraInfo\": null" +
+                                     "}";
+    
+    /// <summary>
+    /// 직렬화/역직렬화할 학생 구조체
+    /// </summary>
+    public readonly struct Student {
+        public string Name { get; }
+        public int Grade { get; }
+        public double LastAvgScore { get; }
+        public string? ExtraInfo { get; }
+        
+        // 변경할 수 없는 타입(여기서는 "읽기 전용 구조체")의 경우,
+        // 생성자에 JsonConstructor 어트리뷰트를 사용해야 역직렬화 가능
+        // 참고: https://learn.microsoft.com/ko-kr/dotnet/standard/serialization/system-text-json/immutability
+        [JsonConstructor]
+        public Student(string name, int grade, double lastAvgScore, string? extraInfo) {
+            Name = name;
+            Grade = grade;
+            LastAvgScore = lastAvgScore;
+            ExtraInfo = extraInfo;
+        }
+
+        public override string ToString() {
+            return $"Student Name: {Name}, Grade: {Grade}, LastAvgScore: {LastAvgScore}, ExtraInfo: {ExtraInfo ?? "(null)"}";
+        }
+    }
+    
+    public static void Main(string[] args) {
+        // JSON 직렬화 옵션 설정 (프로퍼티명이 카멜 케이스임을 지정)
+        var options = new JsonSerializerOptions {
+            PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+        };
+        
+        // JSON 문자열을 plain object로 역직렬화
+        Console.WriteLine("*** Deserialize JSON to plain object");
+        object? student = JsonSerializer.Deserialize<object>(TEST_JSON, options);
+        Console.WriteLine(student);
+        Console.WriteLine();
+        
+        // JSON 문자열을 구조체 타입을 지정하여 역직렬화
+        Console.WriteLine("*** Deserialize JSON to struct");
+        Student typedStudent = JsonSerializer.Deserialize<Student>(TEST_JSON, options);
+        Console.WriteLine(typedStudent);
+        Console.WriteLine();
+        
+        // 코드 상에서 생성된 구조체 객체를 JSON 문자열로 직렬화
+        Console.WriteLine("*** Serialize struct to JSON");
+        Student newStudent = new("Jane", 1, 4.2242d, "Awesome student");
+        string newStudentJson = JsonSerializer.Serialize(newStudent, options);
+        Console.WriteLine(newStudent);
+        Console.WriteLine($" -> {newStudentJson}");
+        Console.WriteLine();
+        
+        // 메모리 스트림으로부터 JSON 문자열을 역직렬화
+        Console.WriteLine("*** Deserialize JSON from memory stream");
+        {
+            using var stream = new MemoryStream(Encoding.UTF8.GetBytes(TEST_JSON));
+            Student streamStudent = JsonSerializer.Deserialize<Student>(stream, options);
+            Console.WriteLine(streamStudent);
+        }
+        Console.WriteLine();
+        
+        // 메모리 스트림에 구조체를 JSON 문자열로 직렬화
+        Console.WriteLine("*** Serialize JSON to memory stream");
+        {
+            using var stream = new MemoryStream();
+            JsonSerializer.Serialize(stream, newStudent, options);
+            Console.WriteLine(Encoding.UTF8.GetString(stream.ToArray()));
+        }
+        Console.WriteLine();
+    }
+}
