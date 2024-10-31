7f5e9dc8a76de78cbdba947b1216c777593294a4
somni <me@somni.one>
2024-10-31 12:50:19 +0900

    2024-2 GAI Week9

---
+++ 2024-2/GAI/Week9/first.py
@@ -0,0 +1,17 @@
+# OneLineComment
+"""
+Hello World Comment
+"""
+
+'''
+Another comment
+'''
+
+print("Hello World")
+hello = "Hellowwwww"
+print(hello)
+
+def func1():
+  print("Hello from func1")
+
+func1()
